46fabd564ccad3b7028b4ef0142dcf6724d58b85
Sagar Khatri <sagark1510@gmail.com>
2018-08-25 19:50:00 +0530

    changes for heroku config

--- server.js
+++ server.js
@@ -26,12 +26,14 @@ require('./server/config/passport')(passport);
 
 // use routes
 app.use('/api/users', users);
+app.use(express.static('client/build'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.get(/^\/(?!api).*/, (req, res) => { // don't serve react app to api routes
+  app.get(/^\/(?!api).*/, (req, res) => {
+    // don't serve react app to api routes
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-};
+}
 
 const port = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send('came 1'));
